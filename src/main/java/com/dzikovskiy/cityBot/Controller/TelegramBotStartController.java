3be0053d5d5967b37400d87f5f31cdef678186c8
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-07-15 17:44:33 +0300

    fix
    fix

--- src/main/java/com/dzikovskiy/cityBot/Controller/TelegramBotStartController.java
+++ src/main/java/com/dzikovskiy/cityBot/Controller/TelegramBotStartController.java
@@ -3,9 +3,10 @@ package com.dzikovskiy.cityBot.Controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
+@RestController
 public class TelegramBotStartController {
 
     @RequestMapping(method = { RequestMethod.GET,
