a15f0d89235c89fa520b28e46e034fb61f06c39f
Dzikovskiy <43319439+Dzikovskiy@users.noreply.github.com>
2020-07-15 18:17:01 +0300

    fix
    fix

---
+++ src/main/java/com/dzikovskiy/cityBot/Controller/TelegramBotStartController.java
@@ -0,0 +1,25 @@
+package com.dzikovskiy.cityBot.Controller;
+
+
+import com.dzikovskiy.cityBot.TelegramBot.TelegramBot;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.Update;
+
+@RestController
+public class TelegramBotStartController {
+
+    private final TelegramBot telegramBot;
+
+    public TelegramBotStartController(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public BotApiMethod<?> onUpdateReceived(@RequestBody Update update) {
+        return telegramBot.onWebhookUpdateReceived(update);
+    }
+}
